7b8d02b8733dcdd2ce96392c3e83501b54c01046
Toossi Hasti <toossiha@teach.cs.toronto.edu>
2023-03-16 02:24:21 -0400

    a3: fixed lru speed issue

--- a3/src/lru.c
+++ a3/src/lru.c
@@ -1,6 +1,9 @@
 #include <assert.h>
 
 #include "pagetable_generic.h"
+#include "sim.h"
+
+struct frame *frame_head; // Head = Most Recently Ref'ed
 
 /* Page to evict is chosen using the accurate LRU algorithm.
  * Returns the page frame number (which is also the index in the coremap)
@@ -9,8 +12,7 @@
 int
 lru_evict(void)
 {
-  assert(false);
-  return -1;
+  return frame_head - coremap;
 }
 
 /* This function is called on each access to a page to update any information
@@ -20,13 +22,22 @@ lru_evict(void)
 void
 lru_ref(int frame)
 {
-  (void)frame;
+  struct frame *frame_node = &coremap[frame];
+  if (frame_head == NULL) { // First to be ref'ed.
+    frame_list_init_head(frame_node);
+  } else if (frame_node != frame_head) {
+    if (frame_node->next != NULL && frame_node->prev != NULL)
+      frame_list_delete(frame_node);
+    frame_list_insert(frame_node, frame_head->prev, frame_head);
+  }
+  frame_head = frame_node;
 }
 
 /* Initialize any data structures needed for this replacement algorithm. */
 void
 lru_init(void)
-{}
+{
+}
 
 /* Cleanup any data structures created in lru_init(). */
 void
